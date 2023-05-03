96621b46f7c812dc0c15759827be1a5aa0e49c3f
Gustavo Castillo <g.castillo_tavo@hotmail.com>
2023-05-02 20:37:15 -0600

    primer commit del proyecto, ya tenemos login

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        
+        User::factory()->create([
+            'name' => 'Tavo',
+            'email' => 'gcastilloo1@miumg.edu.gt',
+            'password' => bcrypt('admin123'), ]);
+
+        User::factory(10)->create();
+    }
+}
---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,131 @@
+{{-- <x-guest-layout>
+    <x-authentication-card>
+        <x-slot name="logo">
+            <x-authentication-card-logo />
+        </x-slot>
+
+        <x-validation-errors class="mb-4" />
+
+        @if (session('status'))
+            <div class="mb-4 font-medium text-sm text-green-600">
+                {{ session('status') }}
+            </div>
+        @endif
+
+        <form method="POST" action="{{ route('login') }}">
+            @csrf
+
+            <div>
+                <x-label for="email" value="{{ __('Email') }}" />
+                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+            </div>
+
+            <div class="mt-4">
+                <x-label for="password" value="{{ __('Password') }}" />
+                <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
+            </div>
+
+            <div class="block mt-4">
+                <label for="remember_me" class="flex items-center">
+                    <x-checkbox id="remember_me" name="remember" />
+                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                </label>
+            </div>
+
+            <div class="flex items-center justify-end mt-4">
+                @if (Route::has('password.request'))
+                    <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
+                        {{ __('Forgot your password?') }}
+                    </a>
+                @endif
+
+                <x-button class="ml-4">
+                    {{ __('Log in') }}
+                </x-button>
+            </div>
+        </form>
+    </x-authentication-card>
+</x-guest-layout> --}}
+
+<x-guest-layout>
+    <div class="grid grid-cols-2 overflow-hidden bg-primary font-raleway"
+    style="background-image: url('{{ asset('./images/maze-black.png') }}')">
+    <div class="max-w-[450px] mx-auto flex flex-col justify-center items-start">
+        <div>
+            <h1 class="mb-5 text-6xl font-bold text-white">Elecciones 2023</h1>
+            @if ($errors->any())
+                <div role="alert">
+                    <div class="px-4 py-2 font-bold text-white bg-red-500 rounded-t">
+                        Error!
+                    </div>
+                    <div class="px-4 py-3 text-red-700 bg-red-100 border border-t-0 border-red-400 rounded-b">
+                        <p>{{ $errors->first() }}</p>
+                    </div>
+                </div>
+            @else
+                <h2 class="text-lg text-white">Inicia sesión para continuar</h2>
+            @endif
+        </div>
+
+        <form method="POST" action="{{ route('login') }}">
+            @csrf
+
+            <div class="gap-4">
+                <div class="mt-10">
+                    <div class="relative">
+                        <input type="email" id="floating_outlined" id="email" :value="old('email')"
+                            name="email" required
+                            class="block w-[350px] px-2.5 pb-2.5 pt-4 text-sm bg-primary rounded-lg border-1 appearance-none text-white border-secondary focus:border-primary_light focus:outline-none focus:ring-0 peer"
+                            placeholder=" " value="{{ old('email') }}">
+                        <label for="floating_outlined"
+                            class="absolute text-sm text-white duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-primary px-2 peer-focus:px-2 peer-focus:text-prborder-primary_light peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1">Correo
+                            Electrónico</label>
+                    </div>
+                </div>
+
+                <div class="mt-10">
+                    <div class="relative">
+                        <input type="password" id="floating_outlined" name="password" required
+                            class="block w-[350px] px-2.5 pb-2.5 pt-4 text-sm bg-primary rounded-lg border-1 appearance-none text-white border-secondary focus:border-primary_light focus:outline-none focus:ring-0 peer"
+                            placeholder=" " />
+                        <label for="floating_outlined"
+                            class="absolute text-sm text-white duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-primary px-2 peer-focus:px-2 peer-focus:text-prborder-primary_light peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1">Contraseña</label>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="flex items-center justify-between w-full mt-10">
+
+                <div class="block">
+                    <label for="remember_me" class="flex items-center">
+                        <input id="remember_me" name="remember" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2">
+                        <span class="ml-2 text-sm text-white">{{ __('Recordarme') }}</span>
+                    </label>
+                </div>
+
+                @if (Route::has('password.request'))
+                    <a class="text-sm underline text-white hover:text-white"
+                        href="{{ route('password.request') }}">
+                        {{ __('¿Ovidaste tu contraseña?') }}
+                    </a>
+                @endif
+            </div>
+
+            <button class="group relative py-3 px-7 overflow-hidden rounded-lg bg-white text-lg shadow mt-5">
+                <div class="absolute inset-0 w-3 bg-button transition-all duration-[250ms] ease-out group-hover:w-full"></div>
+                <span class="relative text-black group-hover:text-white"> Iniciar sesión</span>
+              </button>
+
+        </form>
+    </div>
+
+    <img class="hidden w-full h-screen md:block" src="{{ asset('images/elecciones.jpeg') }}" alt="Imagen de Banner">
+
+
+</div>
+
+
+
+
+</x-guest-layout>
